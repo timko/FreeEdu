275ee47b0223f27c97478e37bb27a422df031c35
Kelvin Chen <kelvin.c.chen@gmail.com>
2012-10-21 19:25:05 -0700

    watch_video feature

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,6 @@
 class Video < ActiveRecord::Base
   attr_accessible :course_num, :size, :time, :name
-  validates :course_num, :presence => true, :uniqueness => true
+  validates :course_num, :presence => true
 
   belongs_to :course
   attr_protected :course_id
--- features/step_definitions/course_steps.rb
+++ features/step_definitions/course_steps.rb
@@ -5,3 +5,18 @@ Given /the following courses exist/ do |courses_table|
     Course.create!(course)
   end
 end
+
+Given /the following videos exist/ do |videos_table|
+  videos_table.hashes.each do |video|
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+    course = Course.find(video[:course_id])
+    video[:course_num] = course.course_num
+    course.videos.create!(video)
+  end
+end
+
+#Then /^I should see the image "(.+)"$/ do |image|
+#    page.should have_xpath("//img[@src=\"/assets/images/#{image}\"]")
+#end
+
