6f0670b71a3eaf95545cf65ab0dd19b75069642e
timko <timmykokopuffs@gmail.com>
2012-10-28 20:50:20 -0700

    added user and settings specs and features

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121021225908) do
+ActiveRecord::Schema.define(:version => 20121029034317) do
 
   create_table "courses", :force => true do |t|
     t.string   "course_num"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(:version => 20121021225908) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "university"
+  end
+
+  create_table "users", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "videos", :force => true do |t|
