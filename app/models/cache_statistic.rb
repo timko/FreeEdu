04af55023d43500d3206a014f818086190dba310
Nyte <saasbook@nyte.(none)>
2012-11-04 14:38:02 -0800

    Cleaning up attr_accessible so that fields are more evident

--- app/models/cache_statistic.rb
+++ app/models/cache_statistic.rb
@@ -1,6 +1,6 @@
 class CacheStatistic < ActiveRecord::Base
-  attr_accessible :log_time, :num_of_users, :bandwidth_demand, :num_of_caches, 
-                    :storage_donated, :bandwidth_donated, :bandwidth_effectively_used, :server_load
+  attr_accessible :log_time, :num_of_users, :bandwidth_demand, :num_of_caches
+  attr_accessible :storage_donated, :bandwidth_donated, :bandwidth_effectively_used, :server_load
   validates :log_time, :uniqueness => true
   def self.extract_sorted_stat(stat_field)
     CacheStatistic.order(:log_time).select(stat_field)
