c6e804a3ba61288317d7dfef17fa5920bf3d259a
Nyte <saasbook@nyte.(none)>
2012-11-04 13:58:59 -0800

    Adding validation on log_time

--- app/models/cache_statistic.rb
+++ app/models/cache_statistic.rb
@@ -1,6 +1,7 @@
 class CacheStatistic < ActiveRecord::Base
   attr_accessible :log_time, :num_of_users, :bandwidth_demand, :num_of_caches, 
                     :storage_donated, :bandwidth_donated, :bandwidth_effectively_used, :server_load
+  validates :log_time, :uniqueness => true
   def self.extract_sorted_stat(stat_field)
     CacheStatistic.order(:log_time).select(stat_field)
   end
