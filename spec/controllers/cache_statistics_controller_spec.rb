baded854b07430d0f9568ec379aeef8392cc9dbf
Nyte <saasbook@nyte.(none)>
2012-11-04 01:57:54 -0700

    Adding more tests in cache_model spec

--- spec/controllers/cache_statistics_controller_spec.rb
+++ spec/controllers/cache_statistics_controller_spec.rb
@@ -30,13 +30,10 @@ describe CacheDataController do
       get :total_stats, :fields => [:field]
       assigns(:total_stats).should == 'data'
     end
-    it 'should return the field data in order of date' do
-      #FactoryGirl should create 3 items in the database unordered by date.
-      #Simulate a field using :field if possible and then use get and assigns to check correctness
-    end
     it 'should return an empty list to the view if there are no valid entries' do
-      #FactoryGirl should create 3 items in the database unordered by date.
-      #Simulate a field using :field if possible and then use get and assigns to check correctness
+      Stats.stub(:find_stats).with(:field).and_return([])
+      get :total_stats, :fields => [:field]
+      assigns(:total_stats)[:field].should == []
     end
   end
 end
