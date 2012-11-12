634eaea4278ce635a8211d6b2cff452059107fc5
timko <timmykokopuffs@gmail.com>
2012-11-11 16:13:22 -0800

    testing code will probably delete

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,6 +4,7 @@ class CoursesController < ApplicationController
   #  @courses: should be an array of all course objects
   def index
     @courses = Course.all
+    @graph = Gchart.line(:data => [[0, 40, 10, 70, 20]])
     @user = User.find(1) #TEMPORARY
   end 
 
