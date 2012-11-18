817be14c139376b5df94e3119c1667ba3a30f5a9
Nyte <saasbook@nyte.(none)>
2012-11-17 18:06:37 -0800

    Adding, will resolve merge conflict

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  
+  def index
+    if session[:user_id]
+      redirect_to tasks_path and return
+    end
+  end
+
+  def create
+    @current_user = User.find_by_name(params[:user][:name])
+    if @current_user and @current_user.authenticate(params[:user][:password]) 
+      session[:user_id] = @current_user.id
+      redirect_to edit_user_path(@current_user.id)
+    else
+      flash[:notice] = "Login/Password not found."  
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'Logged out successfully.'
+    redirect_to root_path
+  end
+end
