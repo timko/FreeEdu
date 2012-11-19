604563e08dea45e9c3d90a46881474a6c83b8b3b
Naoto <naoto@naoto-Lenovo-G550.(none)>
2012-11-18 18:19:55 -0800

    Users_controller_spec more coverage

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,18 +1,15 @@
 require 'spec_helper'
 
 describe UsersController do
-
   describe 'changing the settings' do
     before :each do
-      @fake_result = mock('User')
-      @fake_result.stub(:disk_space).and_return(512)
-      @fake_result.stub(:bandwidth).and_return(160)
-      @fake_result.stub(:id).and_return(1)
+      @fake_result = FactoryGirl.create(:user)
       User.stub(:find).and_return(@fake_result)
       User.should_receive(:find)
-      get :edit, {:id=>@fake_result.id}
+      session[:user_id] = @fake_result.id
+      get :edit
     end
-    it 'should select the Settings edit template for rendering' do
+    it 'should select the Users edit template for rendering' do
       response.should render_template('users/edit')
     end
     it 'should make the settings in the database available to the template' do 
@@ -20,26 +17,23 @@ describe UsersController do
       assigns(:user).disk_space.should == @fake_result.disk_space
       assigns(:user).bandwidth.should == @fake_result.bandwidth
     end
+    it 'should update the settings' do
+      @fake_disk_space = 5
+      @fake_bandwidth = 5
+      post :update, {:user=> {:disk_space => @fake_disk_space, :bandwidth => @fake_bandwidth}, :id => @fake_result.id}
+      assigns(:user).disk_space.should == @fake_disk_space
+      assigns(:user).bandwidth.should == @fake_bandwidth
+    end
     it 'should direct to homepage/settings' do
-      settings_path.should == '/settings/1'
+      settings_path.should == '/settings'
     end
   end
 
-  describe 'adding and deleting a user'do
-    before :each do
-      @fake_user = FactoryGirl.build(:user, :name => 'admin', :password => 'admin', :id => 1)
-    end
-
+  describe 'adding a user'do
     it 'should add a user to the database' do
       before = User.count
-      post :create, :user => @fake_user
-      assert before+1 == User.count
-    end
-    
-    it 'should delete a user to the database' do
-      before = User.count
-      post :destroy, :id => 1
-      assert before-1 == User.count
+      post :create, :user => FactoryGirl.build(:user)
+      (before+1).should == User.count
     end
   end
 
