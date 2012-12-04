e923634b61cf7c026067e798011e5a35263c8509
mrfosip <hamedfallaha@berkeley.edu>
2012-12-03 21:29:43 -0800

    added missing files

---
+++ features/step_definitions/settings-steps.rb
@@ -0,0 +1,12 @@
+Given /^(?:|I )am logged in as "([^"]*)" with password "([^"]*)"$/ do |user, password|
+  step %Q{I follow "Login"}
+	step %Q{I fill in "user_name" with "#{user}"}
+	step %Q{I fill in "user_password" with "#{password}"}
+	step %Q{I press "Sign in"}
+end
+
+Given /^(?:|I )am not logged in as a user$/ do
+	step %Q{I am on the homepage}
+  step %Q{I should see "Login"}
+end
+
