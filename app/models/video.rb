0215f39c0a39e78d371da12f2e3778700211dd32
ThePermaNoob <oh.rlee@berkeley.edu>
2012-12-05 03:28:41 -0800

    fixed bug

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,12 +1,12 @@
 class Video < ActiveRecord::Base
-  attr_protected :course_id
+  
   attr_accessible :name, :source
   validates :source, :presence => true
   validates_uniqueness_of :name, :scope => :course_id
   #validates :name, :uniqueness => {:scope => :course_id}
 
   belongs_to :course
-  
+  attr_protected :course_id
   
   def to_param
     "#{name}".gsub(' ', '_')
