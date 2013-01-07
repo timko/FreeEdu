a2e176179e493dc0339459f0bd39597e6dda0364
Brandon Luong <l.brandon63@gmail.com>
2013-01-07 01:16:37 -0800

    Cleanup

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,9 +1,5 @@
 class Course < ActiveRecord::Base
   attr_accessible :subject, :title, :description
-  #validates :course_num, :presence => true, :uniqueness => true
   validates :title, :presence => true, :uniqueness => true
-
   has_many :videos
-
-
 end
