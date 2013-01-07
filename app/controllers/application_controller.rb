e55bcbeda32ddda94597ec0961d63675610efaf4
Brandon Luong <l.brandon63@gmail.com>
2013-01-07 00:30:54 -0800

    Added comments for application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  #On every webpage, attempt to find the current user
   before_filter :set_current_user
   def set_current_user
     @current_user ||= User.find_by_id session[:user_id]
