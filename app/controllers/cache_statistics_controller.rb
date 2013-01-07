f67ea67953bfb718ef2db65104b35bb5b77eff7b
Brandon Luong <l.brandon63@gmail.com>
2013-01-07 00:31:08 -0800

    Added comments for cache_statistics_controller

--- app/controllers/cache_statistics_controller.rb
+++ app/controllers/cache_statistics_controller.rb
@@ -1,22 +1,27 @@
 class CacheStatisticsController < ApplicationController
 
   def total_stats
+    #Getting updated stats from log file (logs already in the database are not re-added)
     CacheStatistic.create_from_file('app/assets/server_traffic.log', 10)
     @stats = CacheStatistic.get_n_latest_records(20)
     @stats.sort_by! { |stat| stat.log_time}.reverse!
     @stat_descriptions = CacheStatistic.stat_descriptions
 
+    #Selecting the wanted stats
     @selected_stats = params[:stats] || session[:stats] || {}
     @all_stats = CacheStatistic.all_stats.sort
     @real_stat_names = CacheStatistic.stat_names
 
+    #RESTful reinforcement
     if params[:stats] != session[:stats] and @selected_ratings != {}
       session[:stats] = @selected_stats
       redirect_to :stats => @selected_stats and return
+    #If no stats are selected, pretend that all of them are selected.
     elsif @selected_stats == {}
       @selected_stat_names = @all_stats
       @graph = CacheStatistic.get_selected_graph(@all_stats, 10, 20)
       @selected_stat_names.delete('log_time')
+    #Normal case, sort the selected stats and output them
     else
       @selected_stat_names = @selected_stats.keys.sort
       @graph = CacheStatistic.get_selected_graph(@selected_stat_names, 10, 20)
