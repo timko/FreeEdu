753f37358bbe924b9968ac9a68e0404b0d2ba26c
Brandon Luong <l.brandon63@gmail.com>
2013-01-07 00:42:56 -0800

    Added comments to courses_controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,13 +3,22 @@ class CoursesController < ApplicationController
   #you should assign:
   #  @courses: should be an array of all course objects
   def index
+    #Getting updated stats from log file
+    #Logs are added to the database then extracted (Already added logs are not re-logged
     CacheStatistic.create_from_file('app/assets/server_traffic.log', 10)
+    
+    #Showing all courses
     @courses = Course.all
+    
+    #Getting data for server load bar
     latest_stat = CacheStatistic.order("log_time DESC").limit(1)[0]
     load_to_demand = latest_stat.server_load.to_f/ latest_stat.bandwidth_demand
-    @progress_bar_type = "progress progress-"
+    @progress_bar_header = "progress progress-"
+    
+    #Server load percentage
 		@load_to_demand_percentage = load_to_demand*100
     
+    #Color of server load bar
     if load_to_demand <= 0.1
       @server_alert = CacheStatistic.color_alerts['green']
       @progress_bar_type += "info"
